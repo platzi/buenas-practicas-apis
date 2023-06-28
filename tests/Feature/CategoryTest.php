309caab7823e514ffe46bb1bbb88683e9dec0855
Italo Morales F <i@platzi.com>
2023-06-28 10:14:59 -0500

    Api Testing

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+use App\Models\Category;
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
+
+class CategoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_index(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+
+        $categories = Category::factory(2)->create();
+
+        $response = $this->getJson('/api/categories');
+        $response->assertJsonCount(2, 'data')
+            ->assertJsonStructure([
+                'data' => [
+                    [
+                        'id',
+                        'type',
+                        'attributes' => ['name'],
+                    ]
+                ]
+            ]);
+    }
+
+    public function test_show(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+
+        $category = Category::factory()->create();
+
+        $response = $this->getJson('/api/categories/' . $category->id);
+        $response->assertStatus(Response::HTTP_OK) // 200
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'type',
+                    'attributes' => ['name'],
+                ]
+            ]);
+    }
+}
