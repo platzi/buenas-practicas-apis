fb03995d705438cc371608e6e2cf6f54966670d0
Italo Morales F <i@platzi.com>
2023-06-28 10:31:43 -0500

    Testing en tags

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -21,7 +21,8 @@ class CategoryTest extends TestCase
         $categories = Category::factory(2)->create();
 
         $response = $this->getJson('/api/categories');
-        $response->assertJsonCount(2, 'data')
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonCount(2, 'data')
             ->assertJsonStructure([
                 'data' => [
                     [
--- tests/Feature/TagTest.php
+++ tests/Feature/TagTest.php
@@ -3,18 +3,57 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
+
+use App\Models\Tag;
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
 
 class TagTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_index(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+
+        $tags = Tag::factory(2)->create();
+
+        $response = $this->getJson('/api/tags');
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonCount(2, 'data')
+            ->assertJsonStructure([
+                'data' => [
+                    [
+                        'id',
+                        'type',
+                        'attributes' => ['name'],
+                        'relationships' => [
+                            'recipes' => []
+                        ],
+                    ]
+                ]
+            ]);
+    }
+
+    public function test_show(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+
+        $tag = Tag::factory()->create();
+
+        $response = $this->getJson('/api/tags/' . $tag->id);
+        $response->assertStatus(Response::HTTP_OK) // 200
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'type',
+                    'attributes' => ['name'],
+                    'relationships' => [
+                        'recipes' => []
+                    ],
+                ]
+            ]);
     }
 }
