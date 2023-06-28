a6615ff808b90e4cdefe1651b05f54b80488f29d
Italo Morales F <i@platzi.com>
2023-06-28 10:47:42 -0500

    Testing en recipes

--- tests/Feature/RecipeTest.php
+++ tests/Feature/RecipeTest.php
@@ -3,18 +3,68 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
+
+use App\Models\Category;
+use App\Models\Recipe;
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
 
 class RecipeTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_index(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+        
+        Category::factory()->create();
+
+        $recipes = Recipe::factory(2)->create();
+
+        $response = $this->getJson('/api/recipes');
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonCount(2, 'data')
+            ->assertJsonStructure([
+                'data' => [
+                    [
+                        'id',
+                        'type',
+                        'attributes' => ['title', 'description'], // resto de campos
+                    ]
+                ]
+            ]);
+    }
+
+    public function test_show(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+
+        Category::factory()->create();
+
+        $recipe = Recipe::factory()->create();
+
+        $response = $this->getJson('/api/recipes/' . $recipe->id);
+        $response->assertStatus(Response::HTTP_OK) // 200
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'type',
+                    'attributes' => ['title', 'description'], // resto de campos
+                ]
+            ]);
+    }
+
+    public function test_destroy(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+
+        Category::factory()->create();
+
+        $recipe = Recipe::factory()->create();
+
+        $response = $this->deleteJson('/api/recipes/' . $recipe->id);
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
     }
 }
